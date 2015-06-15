1082ed19b2397487787941229c97d0ce53b47946
Aleksey Filippov <sarum9in@gmail.com>
2015-06-15 14:34:32 +0400

    Added bunsan_web_install_skins() and bunsan_web_install_static(), refactored bunsan_web_add_skin().

--- scripts/bunsan_web_add_skin.cmake
+++ scripts/bunsan_web_add_skin.cmake
@@ -1,4 +1,9 @@
-function(bunsan_web_add_skin target)
+macro(bunsan_web_find_cppcms_tmpl_cc)
+    find_program(CPPCMS_TMPL_CC cppcms_tmpl_cc)
+endmacro()
+
+function(bunsan_web_add_skin target skin)
+    bunsan_web_find_cppcms_tmpl_cc()
     set(srcs)
     foreach(src ${ARGN})
         list(APPEND srcs ${CMAKE_CURRENT_SOURCE_DIR}/${src})
@@ -6,12 +11,21 @@ function(bunsan_web_add_skin target)
     set(cpp_target ${CMAKE_CURRENT_BINARY_DIR}/${target}_skin_cpp.cpp)
     add_custom_command(
         OUTPUT ${cpp_target}
-        COMMAND cppcms_tmpl_cc
+        COMMAND ${CPPCMS_TMPL_CC}
             -o ${cpp_target}
             ${srcs}
         DEPENDS ${srcs})
-    add_library(${target} SHARED ${cpp_target})
+    bunsan_add_module_library(${target} ${cpp_target})
+    set_target_properties(${target} PROPERTIES
+                          OUTPUT_NAME ${skin})
+endfunction()
 
-    # bunsan project layout
-    target_include_directories(${target} PRIVATE ${CMAKE_SOURCE_DIR}/include)
+function(bunsan_web_install_skins)
+    bunsan_install_targets_to(${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}/skins
+        ${ARGN}
+    )
+endfunction()
+
+function(bunsan_web_install_static)
+    install(DIRECTORY static DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME})
 endfunction()
